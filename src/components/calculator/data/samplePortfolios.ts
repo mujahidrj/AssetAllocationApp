3b50bf387c50e1814be8a21b92abcbb424c50af5
Mujahid Jaffer <mujahidrj@gmail.com>
2025-05-30 00:55:49 -0700

    Adds Sharia-compliant sample portfolios
    Introduces several Sharia-compliant sample portfolios with varying risk profiles, each containing specific ETFs and their allocations. Enhances support for diverse investment needs.

--- src/components/calculator/data/samplePortfolios.ts
+++ src/components/calculator/data/samplePortfolios.ts
@@ -23,5 +23,63 @@ export const samplePortfolios: SamplePortfolio[] = [
       { name: "SCHF", percentage: 30, companyName: "Schwab International Equity ETF" },
       { name: "SCHZ", percentage: 10, companyName: "Schwab U.S. Aggregate Bond ETF" }
     ]
+  },
+  {
+    name: "Sharia Portfolio Aggressive",
+    stocks: [
+      { name: "SPUS", percentage: 40, companyName: "The SP Funds S&P 500 Sharia Industry Exclusions ETF" },
+      { name: "SPTE", percentage: 40, companyName: "The SP Funds S&P Global Technology ETF" },
+      { name: "SPWO", percentage: 10, companyName: "The SP Funds S&P World ETF" },
+      { name: "SPSK", percentage: 5, companyName: "The SP Funds Dow Jones Global Sukuk ETF" },
+      { name: "SPRE", percentage: 5, companyName: "The SP Funds S&P Global REIT Sharia ETF" }
+    ]
+  },
+  {
+    name: "Sharia Portfolio Growth",
+    stocks: [
+      { name: "SPUS", percentage: 35, companyName: "The SP Funds S&P 500 Sharia Industry Exclusions ETF" },
+      { name: "SPTE", percentage: 30, companyName: "The SP Funds S&P Global Technology ETF" },
+      { name: "SPWO", percentage: 20, companyName: "The SP Funds S&P World ETF" },
+      { name: "SPSK", percentage: 7.5, companyName: "The SP Funds Dow Jones Global Sukuk ETF" },
+      { name: "SPRE", percentage: 7.5, companyName: "The SP Funds S&P Global REIT Sharia ETF" }
+    ]
+  },
+  {
+    name: "Sharia Portfolio Moderate",
+    stocks: [
+      { name: "SPUS", percentage: 30, companyName: "The SP Funds S&P 500 Sharia Industry Exclusions ETF" },
+      { name: "SPTE", percentage: 20, companyName: "The SP Funds S&P Global Technology ETF" },
+      { name: "SPWO", percentage: 20, companyName: "The SP Funds S&P World ETF" },
+      { name: "SPSK", percentage: 20, companyName: "The SP Funds Dow Jones Global Sukuk ETF" },
+      { name: "SPRE", percentage: 10, companyName: "The SP Funds S&P Global REIT Sharia ETF" }
+    ]
+  },
+  {
+    name: "Sharia Portfolio Income",
+    stocks: [
+      { name: "SPSK", percentage: 47.5, companyName: "The SP Funds Dow Jones Global Sukuk ETF" },
+      { name: "SPUS", percentage: 27.5, companyName: "The SP Funds S&P 500 Sharia Industry Exclusions ETF" },
+      { name: "SPTE", percentage: 10, companyName: "The SP Funds S&P Global Technology ETF" },
+      { name: "SPWO", percentage: 10, companyName: "The SP Funds S&P World ETF" },
+      { name: "SPRE", percentage: 5, companyName: "The SP Funds S&P Global REIT Sharia ETF" }
+    ]
+  },
+  {
+    name: "Sharia Portfolio Conservative",
+    stocks: [
+      { name: "SPSK", percentage: 60, companyName: "The SP Funds Dow Jones Global Sukuk ETF" },
+      { name: "SPUS", percentage: 25, companyName: "The SP Funds S&P 500 Sharia Industry Exclusions ETF" },
+      { name: "SPTE", percentage: 10, companyName: "The SP Funds S&P Global Technology ETF" },
+      { name: "SPRE", percentage: 5, companyName: "The SP Funds S&P Global REIT Sharia ETF" }
+    ]
+  },
+  {
+    name: "Sharia Portfolio Sukuk",
+    stocks: [
+      { name: "SPSK", percentage: 65, companyName: "The SP Funds Dow Jones Global Sukuk ETF" },
+      { name: "SPUS", percentage: 17.5, companyName: "The SP Funds S&P 500 Sharia Industry Exclusions ETF" },
+      { name: "SPRE", percentage: 12.5, companyName: "The SP Funds S&P Global REIT Sharia ETF" },
+      { name: "SPTE", percentage: 5, companyName: "The SP Funds S&P Global Technology ETF" }
+    ]
   }
 ];
